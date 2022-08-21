89f46ab774127b07e86bf6a10d365cdce3ce17ac
timurich80 <timurich80@gmail.com>
2022-08-21 15:22:54 +0300

    код работает

---
+++ homework5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ 
+n = n % 10;
+Console.Write("последняя цифра введенного числа:  ");
+Console.WriteLine(n);
