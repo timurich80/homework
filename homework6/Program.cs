72a036b25f58748f000cdfd3d4883b85e2b82a4c
timurich80 <timurich80@gmail.com>
2022-08-21 15:37:30 +0300

    код работает

---
+++ homework6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = -n;
+int count = 0;
+ 
+while (m <= n)
+    {
+    Console.Write(m);
+    Console.Write(", ");
+    m ++;
+    }
