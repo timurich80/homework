31c69f58d10649c201870c9b4362ab5bce711682
timurich80 <timurich80@gmail.com>
2022-08-21 15:07:21 +0300

    код работает

---
+++ homework4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+ 
+while(count <= n)
+{
+    if(count % 2 ==0)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+    
+    }
+    count++;
+}
