ebf32594f5e75f46041d7217e57f8d7c1ee864c3
timurich80 <timurich80@gmail.com>
2022-08-21 12:27:14 +0300

    создали код

---
+++ homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите ваше первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ваше второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{ 
+    Console.Write("max -> ");
+    Console.WriteLine(a);
+}
+ 
+else 
+{
+    Console.Write("max -> ");
+    Console.WriteLine(b);
+}
