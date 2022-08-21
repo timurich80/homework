d105595431885ce6d2e4373108711f21616f8d5d
timurich80 <timurich80@gmail.com>
2022-08-21 12:30:36 +0300

    сщздали код

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Введите ваше первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ваше второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ваше второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+ 
+if(a > b)
+{ 
+    max = a;
+    if(a > c)
+    {
+        max = a;
+        Console.Write("max -> ");
+        Console.WriteLine(a);
+    }
+    else
+    { 
+        max = c;
+        Console.Write("max -> ");
+        Console.WriteLine(c);
+    }
+}
+ 
+else 
+{
+    max = b;
+    if(b > c)
+    {
+        max = b;
+        Console.Write("max -> ");
+        Console.WriteLine(b);
+    }
+ 
+    else
+    { 
+        max = c;
+        Console.Write("max -> ");
+        Console.WriteLine(c);
+    }
+ 
+}
